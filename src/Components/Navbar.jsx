1591ac4fe88ed3d39d4cd196ce4c5e41886be23f
alberkev03 <kevinalberto.62@protonmail.com>
2025-07-27 17:49:06 -0300

    modificado tamaño navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,16 +3,27 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar navbar-light bg-light mb-4 fixed-top d-flex justify-content-end">
-            <nav className="nav flex-column flex-sm-row">
-                <Link to="/proyects">
-                    <button className="btn btn-outline-secondary m-2">
-                        Proyectos
-                    </button>
-                </Link>
-                <Link to="/contact">
-                    <button className="btn btn-primary m-2">Contáctame</button>
-                </Link>
+        <div className="navbar-sm navbar-light bg-light fixed-top">
+            <nav className="nav flex-column flex-sm-row  d-flex justify-content-between">
+                <div>
+                    <Link to="/hero">
+                        <button className="btn btn-outline-primary m-2">
+                            Inicio
+                        </button>
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/proyects">
+                        <button className="btn btn-outline-secondary m-2">
+                            Proyectos
+                        </button>
+                    </Link>
+                    <Link to="/contact">
+                        <button className="btn btn-primary m-2">
+                            Contáctame
+                        </button>
+                    </Link>
+                </div>
             </nav>
         </div>
     );
